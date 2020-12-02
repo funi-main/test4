9b83478fed45144cfc3734ad245a2808974319c5
Juliana de Heer <juliana.deheer@funimation.com>
2020-12-01 22:51:51 -0800

    say_hello takes in a param
    bla
    bla
    bla
    
    Type: api-break
    Jira: TS-000

--- test4/hello.py
+++ test4/hello.py
@@ -1,2 +1,14 @@
-def say_hello():
-    return "Hello Funimation!"
+def say_hello(name):
+    """
+    Say Hello to a given name.
+
+    Args:
+        name: A string representing the name to be greeted.
+
+    Returns:
+        A greeting composed of Hello followed by the name given as param.
+
+    Raises:
+        TypeError: If no params are given, a TypeError is raised.
+    """
+    return f"Hello {name}!"
--- test4/tests/test_hello.py
+++ test4/tests/test_hello.py
@@ -2,5 +2,7 @@ from test4 import hello
 
 
 def test_hello():
-    hello_str = hello.say_hello()
+    ju = "Juliana"
+    hello_str = hello.say_hello(ju)
     assert "Hello" in hello_str
+    assert ju in hello_str
